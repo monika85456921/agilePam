e15405de7c69371572b7f26a18329f40e905433a
Monika <okonecnikovaite.monika@gmail.com>
2024-02-23 08:48:58 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function highlight() {
+  const text = document.getElementById("textInput").value;
+  const search = document.getElementById("searchInput").value;
+  
+  const highlight = new RegExp(search, 'hae')
+  const hightlightTexr = text.replace
+}
